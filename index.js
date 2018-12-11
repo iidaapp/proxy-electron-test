40e326ab755e712ae9b6813d183e22919d2beb21
iidaapp <yuji.iida@l-is-b.com>
2018-12-11 10:21:04 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const { app, BrowserWindow, ipcMain } = require('electron')
+
+let reservedLoginCallback = null
+let window = null
+let proxy = null
+
+app.on('ready', () => {
+    ipcMain.on('finished', (event, {id, password}) => {
+        proxy.close()
+        // This callback doesn't work in second time and doesn't trigger 'login' event, in Electron v3.0.10
+        // But this work well in Electron v2.0.14
+        reservedLoginCallback(id, password)
+        reservedLoginCallback = null
+    })
+    app.on('login', (event, webContents, request, authInfo, callback) => {
+        if (authInfo.isProxy) {
+            event.preventDefault()
+            reservedLoginCallback = callback
+            proxy = new BrowserWindow({width: 400, height: 300})
+            proxy.loadURL(`file://${__dirname}/index.html`)
+        }
+    })
+    window = new BrowserWindow({width: 700, height: 500})
+    window.loadURL('https://www.google.com/')
+})
